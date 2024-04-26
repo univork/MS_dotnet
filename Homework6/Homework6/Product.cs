762eba6de5aa1307d14555256ec0e5b17ad3f241
Cine-Pixel <ikaumphriani@gmail.com>
2024-04-26 19:59:40 +0400

    homework 6

---
+++ Homework6/Homework6/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6
+{
+    public class Product(string title, string description, decimal price, string category)
+    {
+        public string title = title;
+        public string description = description;
+        public decimal price = price;
+        public string category = category;
+    }
+}
--- Homework6/Homework6/Program.cs
+++ Homework6/Homework6/Program.cs
@@ -1,38 +1,21 @@
-﻿using System.Data;
+﻿using Homework6;
+using System.Data;
 using System.Data.SqlClient;
 using System.Security.Cryptography;
 
 string connectionString = "Data Source=KUPH\\SQLEXPRESS;Initial Catalog=store;Integrated Security=SSPI;";
 
 
+// insert
+Product product = new Product("Hammer", "Hammer anyting", 120, "DIY");
+InsertProductWithCategory(product, connectionString);
+
+// read
+ReadProducts(connectionString);
+
+
 using (var conn = new SqlConnection(connectionString))
 {
-    // insert
-    string addProduct = "INSERT into Product (title,description,price, category_id) VALUES (@title,@desc,@price,@category_id)";
-    using (SqlCommand addProductCommand = new(addProduct, conn))
-    {
-        addProductCommand.Parameters.Add("@title", SqlDbType.NVarChar).Value = "Intel Core i9-9900K";
-        addProductCommand.Parameters.Add("@desc", SqlDbType.NVarChar).Value = "Runs anything";
-        addProductCommand.Parameters.Add("@price", SqlDbType.Money).Value = 900;
-        addProductCommand.Parameters.Add("@category_id", SqlDbType.Int).Value = 2;
-        conn.Open();
-        addProductCommand.ExecuteNonQuery();
-        conn.Close();
-    }
-
-
-    // read
-    string readProducts = "SELECT * FROM Product";
-    conn.Open();
-    SqlCommand readCommand = new(readProducts, conn);
-    using (SqlDataReader reader = readCommand.ExecuteReader())
-    {
-        while (reader.Read())
-            Console.WriteLine($"{reader["title"]}");
-    }
-    conn.Close();
-
-
     // update
     string updateProduct = "UPDATE Product SET price = 950 WHERE title = 'Intel Core i9-9900K'";
     using (SqlCommand updateProductCommand = new(updateProduct, conn))
@@ -42,7 +25,6 @@ using (var conn = new SqlConnection(connectionString))
         Console.WriteLine($"{updated_rows} products updated.");
         conn.Close();
     }
-
 
     // delete
     string deleteProduct = "DELETE FROM Product WHERE title = 'Hammer'";
@@ -57,7 +39,7 @@ using (var conn = new SqlConnection(connectionString))
     string readJoinedProducts = "SELECT * FROM Product JOIN Category on Product.category_id = Category.category_id";
     conn.Open();
     SqlCommand readJoinedCommand = new(readJoinedProducts, conn);
-    using (SqlDataReader reader = readCommand.ExecuteReader())
+    using (SqlDataReader reader = readJoinedCommand.ExecuteReader())
     {
         while (reader.Read())
             Console.WriteLine($"{reader["title"]}-{reader["description"]}-{reader["price"]}");
@@ -67,3 +49,53 @@ using (var conn = new SqlConnection(connectionString))
 
     conn.Dispose();
 }
+
+static void ReadProducts(string connection_string)
+{
+    using (var conn = new SqlConnection(connection_string))
+    {
+        string readProducts = "SELECT * FROM Product";
+        conn.Open();
+        SqlCommand readCommand = new(readProducts, conn);
+        using (SqlDataReader reader = readCommand.ExecuteReader())
+        {
+            while (reader.Read())
+                Console.WriteLine($"{reader["title"]}");
+        }
+        conn.Close();
+    }
+}
+
+static void InsertProductWithCategory(Product product, string connection_string)
+{
+    using (var conn = new SqlConnection(connection_string))
+    {
+        conn.Open();
+        SqlTransaction transaction = conn.BeginTransaction();
+        try
+        {
+            string category_query = "INSERT INTO Category (title) VALEUS (@title)";
+            int category_id;
+            using (SqlCommand command = new(category_query, conn, transaction))
+            {
+                command.Parameters.AddWithValue("title", product.category);
+                category_id = (int)command.ExecuteScalar();
+            }
+
+            string product_query = "INSERT into Product (title,description,price, category_id) VALUES (@title,@desc,@price,@category_id)";
+            using(SqlCommand command = new(product_query, conn, transaction))
+            {
+                command.Parameters.AddWithValue("title", product.title);
+                command.Parameters.AddWithValue("desc", product.description);
+                command.Parameters.AddWithValue("price", product.price);
+                command.Parameters.AddWithValue("category_id", category_id);
+                command.ExecuteNonQuery();
+            }
+            Console.WriteLine("Product added");
+        } catch (Exception ex)
+        {
+            transaction.Rollback();
+        }
+    }
+}
+
